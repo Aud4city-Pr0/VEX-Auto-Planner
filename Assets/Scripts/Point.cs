9f03185af14f0a93e3c217dc0a0d97c2b917684b
Supernova4297 <andy.gable2@gmail.com>
2026-04-17 14:50:48 -0400

    Added Point Class
    Created a Point class with x and y coordinates, and static methods to return the distance and angle between 2 points

---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,36 @@
+class Point
+{
+    private double x_coord;
+    private double y_coord;
+
+    public double X_Coord
+    {
+        get { return x_coord; }
+        set { x_coord = value; }
+    }
+
+    public double Y_Coord
+    {
+        get { return y_coord; }
+        set { y_coord = value; }
+    }
+
+    public Point(double x, double y)
+    {
+        x_coord = x;
+        y_coord = y;
+    }
+
+    public static double GetDist(Point a, Point b)
+    {
+        return Math.Sqrt( Math.Pow( a.x_coord - b.x_coord, 2 ) + Math.Pow( a.y_coord - b.y_coord, 2 ) );
+    }
+
+    public static double GetAngle(Point a, Point b)
+    {
+        double x_dif = Math.Abs( a.x_coord - b.x_coord );
+        double y_dif = Math.Abs( a.y_coord - b.y_coord );
+        double angle = Math.Atan( y_dif / x_dif ); // angle in radians
+        return angle*180/Math.PI; // converts to degrees
+    }
+}
