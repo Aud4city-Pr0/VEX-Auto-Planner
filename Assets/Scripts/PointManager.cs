7517f2d4e4aa61b88f5917d24a574d66c1e00097
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-04-17 18:31:51 -0400

    fixed PointManager script

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.Mathematics;
 public class PointManager : MonoBehaviour
 {
     public static List<Point> points = new List<Point> {};
 
-    public static void AddPoint(double x, double y)
+    public static void AddPoint(float x, float y)
     {
-        points.Add(new Point(x, y));
-        Debug.Log($"Added new point at: {x, y}");
+        points.Add(new Point((double)x, (double)y));
+        Debug.Log("Added new point at");
     }
 
     public static void ChangePoint(int pointIndex, double x, double y)
     {
-        points[pointIndex].X_Coord.set(x);
-        points[pointIndex].Y_Coord.set(y);
-        Debug.Log($"changed point at: i: {pointIndex} p: {x, y}");
+        points[pointIndex].X_Coord = y;
+        points[pointIndex].Y_Coord = x;
     }
 
     public static void DeletePoint(int pointIndex)
