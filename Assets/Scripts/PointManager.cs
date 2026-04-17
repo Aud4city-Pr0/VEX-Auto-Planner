a5cff21fd6f6f48858065683b6786da7eb651775
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-04-17 15:27:43 -0400

    last changes of the day

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
-using System;
-public class PointManager : MonoBehavior
+using System.Collections.Generic;
+public class PointManager : MonoBehaviour
 {
     public static List<Point> points = new List<Point> {};
 
     public static void AddPoint(double x, double y)
     {
         points.Add(new Point(x, y));
+        Debug.Log($"Added new point at: {x, y}");
     }
 
     public static void ChangePoint(int pointIndex, double x, double y)
     {
         points[pointIndex].X_Coord.set(x);
         points[pointIndex].Y_Coord.set(y);
+        Debug.Log($"changed point at: i: {pointIndex} p: {x, y}");
     }
 
     public static void DeletePoint(int pointIndex)
     {
         points.RemoveAt(pointIndex);
+        Debug.Log($"removed point at: i: {pointIndex}");
     }
 }
